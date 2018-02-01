42434d7c41d256ee6d3bffff61ce311fcf592ccd
Ehud <Ehud@Ehud-PC>
2018-02-01 23:44:42 +0200

    first commit

---
+++ CRMServer/src/test/java/com/ehud/levy/CrmServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ehud.levy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CrmServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
